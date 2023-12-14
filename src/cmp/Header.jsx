d1b4d989f411b2c47e6edceb17b4bd0cb78ca37b
imokiswear <oreismatheus@gmail.com>
2023-12-14 17:04:47 -0300

    fix: send the icon.png to the 'public' folder

--- src/cmp/Header.jsx
+++ src/cmp/Header.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import styles from "./Header.module.css";
 
+import logo from "/icon.png";
+
 const Header = () => {
   return (
     <header className={ styles.base } >
         <div className={ styles.fixed } >
             <a href='#top' className={ styles.button }>
-                <img src="./src/assets/icon.png" alt="" width={32} height={32}/>
+                <img src={logo} alt="" width={32} height={32}/>
             </a>
         </div>
     </header>
